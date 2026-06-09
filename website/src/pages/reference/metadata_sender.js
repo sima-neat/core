c705b8a637ccaf1c19fc4383d0f32da613169bb0
Florian Voss <florian.voss@sima.ai>
2026-06-09 14:22:01 +0000

    delete compatibility stubs

--- website/src/pages/reference/metadata_sender.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import {Redirect} from "@docusaurus/router";
-
-export default function MetadataSenderRedirect() {
-  return <Redirect to="/develop-apps/advanced-concepts/metadata_sender/" />;
-}
--- website/src/pages/reference/video_sender.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import {Redirect} from "@docusaurus/router";
-
-export default function VideoSenderRedirect() {
-  return <Redirect to="/develop-apps/advanced-concepts/video_sender/" />;
-}
