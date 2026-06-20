8ff9df976b33c761924322011823b5d0093916dc
Florian Voss <florian.voss@sima.ai>
2026-06-20 09:08:09 +0000

    relax platform requirements for extras

--- build.sh
+++ build.sh
@@ -2662,8 +2662,7 @@ generate_package_metadata_if_requested() {
     --exclude ".sh" \
     --exclude ".txt" \
     --exclude ".json" \
-    --variant extras \
-    "${package_compatibility_args[@]}"
+    --variant extras 
 
   WHEEL_BASENAME="$(basename "${wheel_path}")" python3 - <<'PY'
 import json
