046f818bf9a30e9ad2dd1680c4284bbad80a2ac1
Onat Inak <onat.inak@sima.ai>
2026-07-23 01:10:31 +0200

    feat(video): add H.265 RTP passthrough

--- src/nodes/groups/VideoSender.cpp
+++ src/nodes/groups/VideoSender.cpp
@@ -8,6 +8,7 @@
 #include "nodes/sima/H264Parse.h"
 
 #include <memory>
+#include <sstream>
 #include <stdexcept>
 #include <string>
 #include <utility>
@@ -15,6 +16,71 @@
 
 namespace simaai::neat::nodes::groups {
 namespace {
+
+class H265ParseNode final : public simaai::neat::Node, public simaai::neat::OutputSpecProvider {
+public:
+  explicit H265ParseNode(int config_interval) : config_interval_(config_interval) {}
+
+  std::string kind() const override {
+    return "H265Parse";
+  }
+  NodeCapsBehavior caps_behavior() const override {
+    return NodeCapsBehavior::Dynamic;
+  }
+  std::string backend_fragment(int node_index) const override {
+    return "h265parse name=n" + std::to_string(node_index) +
+           "_h265parse disable-passthrough=true config-interval=" +
+           std::to_string(config_interval_);
+  }
+  std::vector<std::string> element_names(int node_index) const override {
+    return {"n" + std::to_string(node_index) + "_h265parse"};
+  }
+  simaai::neat::OutputSpec output_spec(const simaai::neat::OutputSpec&) const override {
+    simaai::neat::OutputSpec out;
+    out.media_type = "video/x-h265";
+    out.format = "H265";
+    out.certainty = simaai::neat::SpecCertainty::Hint;
+    out.note = "H265 parse output";
+    return out;
+  }
+
+private:
+  int config_interval_ = 1;
+};
+
+class H265PacketizeNode final : public simaai::neat::Node, public simaai::neat::OutputSpecProvider {
+public:
+  H265PacketizeNode(int payload_type, int config_interval)
+      : payload_type_(payload_type), config_interval_(config_interval) {}
+
+  std::string kind() const override {
+    return "H265Packetize";
+  }
+  NodeCapsBehavior caps_behavior() const override {
+    return NodeCapsBehavior::Dynamic;
+  }
+  std::string backend_fragment(int) const override {
+    std::ostringstream fragment;
+    fragment << "rtph265pay name=pay0 pt=" << payload_type_
+             << " config-interval=" << config_interval_ << " timestamp-offset=0";
+    return fragment.str();
+  }
+  std::vector<std::string> element_names(int) const override {
+    return {"pay0"};
+  }
+  simaai::neat::OutputSpec output_spec(const simaai::neat::OutputSpec&) const override {
+    simaai::neat::OutputSpec out;
+    out.media_type = "application/x-rtp";
+    out.format = "H265";
+    out.certainty = simaai::neat::SpecCertainty::Hint;
+    out.note = "RTP H265 payload";
+    return out;
+  }
+
+private:
+  int payload_type_ = 98;
+  int config_interval_ = 1;
+};
 
 void require_positive(int value, const char* name) {
   if (value <= 0) {
@@ -28,6 +94,20 @@ void append_h264_rtp_udp_nodes(std::vector<std::shared_ptr<simaai::neat::Node>>&
   nodes.push_back(
       nodes::H264Packetize(simaai::neat::H264Packetize::PayloadType(opt.rtp.payload_type),
                            simaai::neat::H264Packetize::ConfigInterval(opt.rtp.config_interval)));
+
+  simaai::neat::UdpOutputOptions udp_opt;
+  udp_opt.host = opt.host;
+  udp_opt.port = opt.video_port();
+  udp_opt.sync = opt.sync;
+  udp_opt.async = opt.async;
+  nodes.push_back(nodes::UdpOutput(udp_opt));
+}
+
+void append_h265_rtp_udp_nodes(std::vector<std::shared_ptr<simaai::neat::Node>>& nodes,
+                               const VideoSenderOptions& opt) {
+  nodes.push_back(std::make_shared<H265ParseNode>(opt.rtp.config_interval));
+  nodes.push_back(
+      std::make_shared<H265PacketizeNode>(opt.rtp.payload_type, opt.rtp.config_interval));
 
   simaai::neat::UdpOutputOptions udp_opt;
   udp_opt.host = opt.host;
@@ -58,6 +138,13 @@ VideoSenderOptions VideoSenderOptions::H264RtpUdpFromEncoded() {
   return opt;
 }
 
+VideoSenderOptions VideoSenderOptions::H265RtpUdpFromEncoded() {
+  VideoSenderOptions opt;
+  opt.input_kind_ = InputKind::EncodedH265;
+  opt.rtp.payload_type = 98;
+  return opt;
+}
+
 simaai::neat::Graph VideoSender(const VideoSenderOptions& opt) {
   std::vector<std::shared_ptr<simaai::neat::Node>> nodes;
   nodes.reserve(opt.is_raw_input() ? 7 : 3);
@@ -73,7 +160,11 @@ simaai::neat::Graph VideoSender(const VideoSenderOptions& opt) {
                                           opt.encoder.level));
   }
 
-  append_h264_rtp_udp_nodes(nodes, opt);
+  if (opt.input_kind_ == VideoSenderOptions::InputKind::EncodedH265) {
+    append_h265_rtp_udp_nodes(nodes, opt);
+  } else {
+    append_h264_rtp_udp_nodes(nodes, opt);
+  }
 
   simaai::neat::Graph graph("video_sender");
   for (auto& node : nodes) {
